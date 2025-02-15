3b923da42fd01ebbb1969fc4cf0c1f5c2763e7b0
rieaaddlreiuu <kg21428ks@isk.ed.jp>
2025-02-15 23:20:32 +0900

    ふぁいるをつくった

---
+++ scripts/showRoleSelectionForm.js
@@ -0,0 +1,6 @@
+import { world, system, Player, ItemStack, ItemUseAfterEvent } from "@minecraft/server";
+import { ModalFormData, ModalFormResponse, ActionFormData, ActionFormResponse } from "@minecraft/server-ui";
+
+export function showRoleSelectionForm(player) {
+
+}
