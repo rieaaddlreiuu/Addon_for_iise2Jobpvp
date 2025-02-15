e0d47a0089def74ada9a545c76a1e9425d9737ca
rieaaddlreiuu <kg21428ks@isk.ed.jp>
2025-02-15 20:06:14 +0900

    warriorのkyouka2

--- scripts/role_behaviors/warrior.js
+++ scripts/role_behaviors/warrior.js
@@ -1,4 +1,4 @@
-import { world, system, Player, ItemStack, ItemUseAfterEvent } from "@minecraft/server";
+import { world, system, Player, ItemStack, ItemUseAfterEvent, EntityHealthComponent, EntityComponentTypes } from "@minecraft/server";
 import { ModalFormData, ModalFormResponse, ActionFormData, ActionFormResponse } from "@minecraft/server-ui";
 
 function runWithProbability(probability, func) {
@@ -7,18 +7,24 @@ function runWithProbability(probability, func) {
     }
 }
 
+function Damage(entity,power) {
+    const health = entity.getComponent(EntityComponentTypes.Health);
+    health.setCurrentValue(health.currentValue - power);
+}
+
+
 
 export function warrior_behavior() {
     world.afterEvents.entityHurt.subscribe(data => {
         let player = data.damageSource.damagingEntity;
         let hurtEntity = data.hurtEntity;
         let damage_amount = data.damage;
-        let power = damage_amount * 5;
+        let power = damage_amount * damage_amount;
         if (player.hasTag("jobpvp_role_warrior")) {
             runWithProbability(0.3, function () {
-                player.runCommand("playsound random.anvil_land @p");
+                player.runCommand("playsound mace.heavy_smash_ground @p");
                 system.runTimeout(() => {
-                    hurtEntity.applyDamage(power, { cause: "selfDestruct" });
+                    Damage(hurtEntity,power);
                     hurtEntity.runCommand("particle minecraft:smash_ground_particle_center ~~~");
                     hurtEntity.runCommand("particle minecraft:knockback_roar_particle ~~~");
                     hurtEntity.runCommand("particle minecraft:knockback_roar_particle ~~~");
